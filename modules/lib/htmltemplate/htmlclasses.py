03ce76bbd3e524c521059999ec4aeb564440025d
Nriver <6752679+Nriver@users.noreply.github.com>
2022-08-03 14:39:54 +0800

    js和css静态文件存储到设备上，不使用cdn

--- modules/lib/htmltemplate/htmlclasses.py
+++ modules/lib/htmltemplate/htmlclasses.py
@@ -8,7 +8,7 @@ from htmltemplate.template import Template
 # <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 # <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 # <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-beg_tagStylesheet = b'''<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>'''
+beg_tagStylesheet = b'''<link rel="stylesheet" href="bootstrap.min.css"><script src="jquery.min.js"></script><script src="popper.min.js"></script><script src="bootstrap.min.js"></script>'''
 def Stylesheet(*args, **params):
 	self = Template(*(("Stylesheet",) + args), **params)
 
