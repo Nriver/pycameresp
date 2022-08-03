1df9bbc6b108be2abb8f2b66500a3a958435c845
Nriver <6752679+Nriver@users.noreply.github.com>
2022-08-03 14:40:29 +0800

    静态文件写入到存储

--- scripts/patchInisetup.py
+++ scripts/patchInisetup.py
@@ -1,40 +1,42 @@
 
 filesToAdd = [
-	("","modules/main.py"),
-	("","modules/pycameresp.py"),
-	("","modules/sample.py"),
+    ("", "modules/main.py"),
+    ("", "modules/pycameresp.py"),
+    ("", "modules/sample.py"),
 ]
 
 if __name__ == "__main__":
-	from sys import argv
-	if len(argv) > 1:
-		root = argv[1]
-	else:
-		root = "firmware"
+    from sys import argv
+    if len(argv) > 1:
+        root = argv[1]
+    else:
+        root = "firmware"
 
-	fileSetup='''
+    fileSetup = '''
     with open("%(filename)s", "w") as f:
         f.write("""%(content)s""")
 '''
-	from os.path import split
-	patchIni=""
-	import glob
-	for filename in glob.glob("modules/www/*.css"):
-		if not "bootstrap" in filename:
-			filesToAdd.append(("www",filename))
-	for path, filename in filesToAdd:
-		content = open(filename,"r").read()
-		content = content.replace('"""',"'''")
-		content = content.replace("\x5C","\x5C\x5C") # Replace \ by \\
-		if path != "":
-			patchIni += "    try:\n        uos.mkdir('%s')\n    except: pass\n"%path
-			filename = "%s/%s"%(path, split(filename)[1])
-		else:
-			filename = split(filename)[1]
-		patchIni += fileSetup%locals()
+    from os.path import split
+    patchIni = ""
+    import glob
+    # dirty hack
+    for filename in glob.glob("modules/www/*"):
+        # 要排除掉这个文件，不然会报错
+        if not 'template.html' in filename:
+            filesToAdd.append(("www", filename))
+    for path, filename in filesToAdd:
+        content = open(filename, "r").read()
+        content = content.replace('"""', "'''")
+        content = content.replace("\x5C", "\x5C\x5C")  # Replace \ by \\
+        if path != "":
+            patchIni += "    try:\n        uos.mkdir('%s')\n    except: pass\n" % path
+            filename = "%s/%s" % (path, split(filename)[1])
+        else:
+            filename = split(filename)[1]
+        patchIni += fileSetup % locals()
 
-	inisetup = open("patch/python/micropython/ports/esp32/modules/inisetup.py","r").read()
-	open(root + "/micropython/ports/esp32/modules/inisetup.py","w").write(inisetup%patchIni)
-	import time
-	year,month,day,hour,minute,second,weekday,yearday = time.localtime()[:8]
-	open("modules/lib/tools/builddate.py","w").write("date=b'%04d/%02d/%02d  %02d:%02d:%02d'\n"%(year,month,day,hour,minute,second))
+    inisetup = open("patch/python/micropython/ports/esp32/modules/inisetup.py", "r").read()
+    open(root + "/micropython/ports/esp32/modules/inisetup.py", "w").write(inisetup % patchIni)
+    import time
+    year, month, day, hour, minute, second, weekday, yearday = time.localtime()[:8]
+    open("modules/lib/tools/builddate.py", "w").write("date=b'%04d/%02d/%02d  %02d:%02d:%02d'\n" % (year, month, day, hour, minute, second))
--- scripts/zip_mpy.py
+++ scripts/zip_mpy.py
@@ -1,45 +1,48 @@
+import os.path
+from zipfile import ZipFile, ZIP_DEFLATED 
 import sys
 import useful
 
-ROOT=sys.argv[1]
-BOARD=sys.argv[2]
-PYCAMERESP=sys.argv[3]
-MPY_DIRECTORY="%(ROOT)s/micropython/ports/esp32/build-%(BOARD)s/frozen_mpy/"%globals()
-PY_DIRECTORY="%(ROOT)s/micropython/ports/esp32/modules/"%globals()
+ROOT = sys.argv[1]
+BOARD = sys.argv[2]
+PYCAMERESP = sys.argv[3]
+MPY_DIRECTORY = "%(ROOT)s/micropython/ports/esp32/build-%(BOARD)s/frozen_mpy/" % globals()
+PY_DIRECTORY = "%(ROOT)s/micropython/ports/esp32/modules/" % globals()
 excludeds = [
-	"_boot.*",
-	"apa106.*",
-	"flashbdev.*",
-	"dht.*",
-	"ds18x20.*",
-	"neopixel.*",
-	"ntptime.*",
-	"onewire.*",
-	"upip_utarfile.*",
-	"upip.*",
-	"webrepl_setup.*",
-	"webrepl.*",
-	"websocket_helper.*",
-	"inisetup.*",
-	"*/motion/*",
-	"*/video/*",
-	"*/uasyncio/*"]
+    "_boot.*",
+    "apa106.*",
+    "flashbdev.*",
+    "dht.*",
+    "ds18x20.*",
+    "neopixel.*",
+    "ntptime.*",
+    "onewire.*",
+    "upip_utarfile.*",
+    "upip.*",
+    "webrepl_setup.*",
+    "webrepl.*",
+    "websocket_helper.*",
+    "inisetup.*",
+    "*/motion/*",
+    "*/video/*",
+    "*/uasyncio/*"]
 
 excludeds_shell = excludeds + [
-	"*/webpage/*",
-	"*/server/*",
-	"*/wifi/*",
-	"*/htmltemplate/*"]
+    "*/webpage/*",
+    "*/server/*",
+    "*/wifi/*",
+    "*/htmltemplate/*"]
 
-useful.zip_dir("%s/delivery/shell.zip"%PYCAMERESP,MPY_DIRECTORY,  ["*.mpy"], excludeds_shell, False, [["frozen_mpy","lib"]])
+useful.zip_dir("%s/delivery/shell.zip" % PYCAMERESP, MPY_DIRECTORY, ["*.mpy"], excludeds_shell, False, [["frozen_mpy", "lib"]])
 
-from zipfile import ZipFile, ZIP_DEFLATED 
-import os.path
-useful.zip_dir("%s/delivery/server.zip"%PYCAMERESP,MPY_DIRECTORY, ["*.mpy"], excludeds      , False, [["frozen_mpy","lib"]])
-z = ZipFile("%s/delivery/server.zip"%PYCAMERESP, "a",ZIP_DEFLATED)
-z.write(os.path.normpath("%s/modules/main.py"%PYCAMERESP),"main.py")
-z.write(os.path.normpath("%s/modules/pycameresp.py"%PYCAMERESP),"pycameresp.py")
-z.write(os.path.normpath("%s/modules/www/stylesheet.css"%PYCAMERESP),"www/stylesheet.css")
+useful.zip_dir("%s/delivery/server.zip" % PYCAMERESP, MPY_DIRECTORY, ["*.mpy"], excludeds, False, [["frozen_mpy", "lib"]])
+z = ZipFile("%s/delivery/server.zip" % PYCAMERESP, "a", ZIP_DEFLATED)
+z.write(os.path.normpath("%s/modules/main.py" % PYCAMERESP), "main.py")
+z.write(os.path.normpath("%s/modules/pycameresp.py" % PYCAMERESP), "pycameresp.py")
+z.write(os.path.normpath("%s/modules/www/bootstrap.min.css" % PYCAMERESP), "www/bootstrap.min.css")
+z.write(os.path.normpath("%s/modules/www/bootstrap.min.js" % PYCAMERESP), "www/bootstrap.min.js")
+z.write(os.path.normpath("%s/modules/www/jquery.min.js" % PYCAMERESP), "www/jquery.min.js")
+z.write(os.path.normpath("%s/modules/www/popper.min.js" % PYCAMERESP), "www/popper.min.js")
 z.close()
 
-useful.zip_dir("%s/delivery/editor.zip"%PYCAMERESP,PY_DIRECTORY,  ["*/editor*.py","*/filesystem.py","*/jsonconfig.py","*/terminal.py","*/logger.py","*/useful.py","*/strings.py","*/fnmatch.py"],[], False, [["shell","editor"],["tools","editor"],["modules",""]])
+useful.zip_dir("%s/delivery/editor.zip" % PYCAMERESP, PY_DIRECTORY, ["*/editor*.py", "*/filesystem.py", "*/jsonconfig.py", "*/terminal.py", "*/logger.py", "*/useful.py", "*/strings.py", "*/fnmatch.py"], [], False, [["shell", "editor"], ["tools", "editor"], ["modules", ""]])
